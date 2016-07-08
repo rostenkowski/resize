31e3369e91e06302a5e0eab66c3370ef6774ac70
Bernardette Maryan Rostenkowski <rostenkowski@users.noreply.github.com>
2016-07-09 01:31:14 +0200

    tests: setup script

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
 # add repository
-sudo add-apt-repository ppa:ondrej/php5-5.6
+sudo add-apt-repository ppa:ondrej/php
 
-# install php latest 5.6
-sudo apt-get update && sudo apt-get install \
-	python-software-properties \
-	imagemagick \
-	php5-cli \
-	php5-cgi \
-	php5-curl \
-	php5-imagick \
-	php5-gd
+# install php
+sudo apt-get update && \
+sudo apt-get install \
+	php-cli \
+	php-cgi \
+	php-curl \
+	php-gd \
+	php-imagick \
+	php-mbstring
 
 # composer
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
-composer install -o --prefer-dist
+
+composer update -o --prefer-dist
