7febb5732bdedbae5205d128e5504dff8730f77b
Bernardette Maryan Rostenkowski <rostenkowski@users.noreply.github.com>
2016-07-09 01:30:37 +0200

    tests: testing store and cache directories are wiped out before running test suite (after failure)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,4 +6,10 @@ use Tester\Environment;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+// reset testing directories
+$storeDir = __DIR__ . '/store';
+$cacheDir = __DIR__ . '/cache';
+exec(sprintf('rm -rf %s', escapeshellarg($storeDir)));
+exec(sprintf('rm -rf %s', escapeshellarg($cacheDir)));
+
 Environment::setup();
