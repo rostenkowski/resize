5fe9ad883b3a1a67efb17a7926a069c442b9e8e4
rostenkowski <svodev@email.cz>
2017-03-25 13:05:29 +0100

    php7

--- src/ImageStore/Extension.php
+++ src/ImageStore/Extension.php
@@ -31,6 +31,8 @@ class Extension extends CompilerExtension
 
 		$builder = $this->getContainerBuilder();
 
+		Debugger::barDump($this->options, 'imagestore options');
+
 		$builder->addDefinition($this->prefix('storage'))
 			->setClass($this->options['storageClass'], [
 				$this->options['storageDir'],
